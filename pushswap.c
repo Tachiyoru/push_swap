b00b768b9fcf5f7f2b00d8bf65a5a623a3e403e5
Shanley LEON <sleon@e2r8p3.clusters.42paris.fr>
2022-10-04 12:22:49 +0200

    freed

--- pushswap.c
+++ pushswap.c
@@ -6,30 +6,29 @@
 /*   By: sleon <sleon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:28:53 by sleon             #+#    #+#             */
-/*   Updated: 2022/09/30 15:24:46 by sleon            ###   ########.fr       */
+/*   Updated: 2022/10/04 12:19:05 by sleon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
 
-void	print_list(t_list *stack)
-{
-	if (!stack)
-		return ;
-	printf("\n");
-	while (stack)
-	{
-		printf("%d\n", stack->content);
-		printf(" pack = %d\n", stack->pack);
-		stack = stack->next;
-	}
-	printf("\n");
-}
-// a mettre dans main
-	// printf("les listes a la fin de tout : \n");
-	// print_list(*stack_a);
-	// printf("stack b : \n");
-	// print_list(*stack_b);
+// void	print_list(t_list *stack)
+// {
+// 	if (!stack)
+// 		return ;
+// 	printf("\n");
+// 	while (stack)
+// 	{
+// 		printf("%d\n", stack->content);
+// 		printf(" pack = %d\n", stack->pack);
+// 		stack = stack->next;
+// 	}
+// 	printf("\n");
+// }
+// printf("les listes a la fin de tout : \n");
+// print_list(*stack_a);
+// printf("stack b : \n");
+// print_list(*stack_b);
 
 int	main(int argc, char **argv)
 {
@@ -40,7 +39,6 @@ int	main(int argc, char **argv)
 		return (0);
 	stack_a = malloc(sizeof(t_list *));
 	stack_b = malloc(sizeof(t_list *));
-	*stack_b = malloc(sizeof(t_list *));
 	*stack_b = NULL;
 	*stack_a = args_to_stack(argc, argv);
 	if (! *stack_a)
